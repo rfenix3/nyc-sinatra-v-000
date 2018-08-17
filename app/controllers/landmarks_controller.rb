1c149dd4d9c0939354b3b6c424f76a7c5a3130d7
Richard Fenix <richard.fenix@gmail.com>
2018-08-17 14:09:31 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,28 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get 'landmarks/:id' do
+    #show.erb
+  end
+
+  get '/landmarks/:id/edit' do
+    #binding.pry
+    @landmark = Landmark.find(params["id"])
+    erb :'landmark/edit'
+  end
+
+  post '/landmarks' do
+    binding.pry
+    @landmark = Landmark.create(params["landmark"])
+    redirect "/landmarks/#{@figure.id}"
+  end
+
 end
